c59c00eafec19001ce7c559a22f449d0099345d5
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2021-11-18 16:12:06 +0000

    Add component descriptors

---
+++ src/main/java/org/creek/api/platform/metadata/ComponentDescriptor.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 Creek Contributors (https://github.com/creek-service)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.creek.api.platform.metadata;
+
+
+import java.util.Collection;
+import java.util.List;
+
+/** Defines metadata about a platform component */
+public interface ComponentDescriptor {
+
+    /** @return the unique name of the component within the platform */
+    String name();
+
+    /** @return the inputs to the component, e.g. Kafka topics it consumes. */
+    default Collection<ComponentInput> inputs() {
+        return List.of();
+    }
+
+    /** @return the internals to the component, e.g. changelog or repartition Kafka topics. */
+    default Collection<ComponentInternal> internals() {
+        return List.of();
+    }
+
+    /** @return the outputs from the component, e.g. the Kafka topics it outputs too */
+    default Collection<ComponentOutput> outputs() {
+        return List.of();
+    }
+}
---
+++ src/main/java/org/creek/api/platform/metadata/ServiceDescriptor.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 Creek Contributors (https://github.com/creek-service)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.creek.api.platform.metadata;
+
+
+import java.util.Map;
+
+/** Type defining metadata data about a service. */
+public interface ServiceDescriptor extends ComponentDescriptor {
+
+    /**
+     * The simple name of the docker image that contains the service.
+     *
+     * <p>Given a full image name of {@code confluentinc/cp-kafka:6.1.2}, this method should return
+     * {@code cp-kafka}.
+     *
+     * @return the service's docker image name.
+     */
+    default String dockerImage() {
+        return name();
+    }
+
+    /**
+     * Allows customisation of the environment variables available to the service during system
+     * testing.
+     */
+    default Map<String, String> testEnvironment() {
+        return Map.of();
+    }
+}
